33e768e1189c81c069c21496626d6afaf5f43af0
Robert Johnson <rcj148@gmail.com>
2021-12-21 12:51:47 -0500

    updates

--- WebFundDec2021/w1d5/Core/Button Clicker/script.js
+++ WebFundDec2021/w1d5/Core/Button Clicker/script.js
@@ -8,3 +8,11 @@ function clickLike(element){
     count++;
     element.innerText = count + " likes";
 }
+
+var count = 0
+
+var btnRe = button.getElementById("btnR").removeAttribute("onclick");
+
+if(btnRe.onclick == true){
+    this.onclick = null;
+}
