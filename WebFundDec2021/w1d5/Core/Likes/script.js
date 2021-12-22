fa260fe7241dfa78f415744e752ecbb230c0592b
Robert Johnson <rcj148@gmail.com>
2021-12-22 10:47:17 -0500

    updates

--- WebFundDec2021/w1d5/Core/Likes/script.js
+++ WebFundDec2021/w1d5/Core/Likes/script.js
@@ -1,8 +1,22 @@
 var count = 0;
+var count1 = 0;
+var count2 = 0;
+var likeCount = document.querySelector("#likeCount");
 
-function clickLike("addTo"){
+function clickLike(){
+
     count++;
-    addTo = count + " like(s)";
+    likeCount1.innerText = count + " like(s)";
+}
 
+function clickLike1(){
+    // console.log("does this work")
+    count1++;
+    likeCount2.innerText = count1 + " like(s)";
+}
 
+function clickLike2(){
+    // console.log("working??")
+    count2++;
+    likeCount3.innerText = count2 + " like(s)";
 }
